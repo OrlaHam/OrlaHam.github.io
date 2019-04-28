ed96c7e332ef51d8ba08919d98c3887f573dc997
OrlaHam <orla.m.hammond@gmail.com>
2019-04-28 17:26:47 +1200

    Adding Map
    Adding the Leaflet Code to add the base map to the page.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var map = L.map('map', {
+    center: [-41.288,174.777],
+    zoom: 14,
+//    minZoom: 3,
+//    maxZoom: 14,
+});
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+//		minZoom: 3,
+//      maxZoom: 14,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+}).addTo(map);
